d48fabae49dc1c70b5aa8d314d819fc359ba4d06
LafTools Team <work7z@outlook.com>
2023-11-29 00:40:11 +0800

    🍀Provide scan-entry.js file

--- dev/scripts/scan/scan-entry.js
+++ dev/scripts/scan/scan-entry.js
@@ -1,33 +1,44 @@
-console.log("working it");
+console.log("start scanning it");
 let path = require('path')
 let fs = require('fs')
+var md5 = require('md5');
 
-function beGOOD(idx) {
+function sub_exp(idx) {
   return "((?<![\\\\])['\"`])((?:.(?!(?<![\\\\])\\1))*.?)\\" + idx;
 }
 
 let commonText = new RegExp(
-  "Dot\\s*\\(\\s*" + beGOOD(1) + "\\s*,\\s*" + beGOOD(3)
+  "Dot\\s*\\(\\s*" + sub_exp(1) + "\\s*,\\s*" + sub_exp(3)
 );
 
 // get env LAFTOOLS_ROOT
 let baseDIR = process.env.LAFTOOLS_ROOT;
-function getFile(filePath) {
-  return (filePath);
+// sleep 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
 }
 function readFileAsJSONMap(file) {
-  return JSON.parse(fs.readFileSync(file,{encoding:'utf-8'}).toString());
+  return JSON.parse(file);
+}
+function getFile(file) {
+  let obj = {
+    file,
+    text: () => {
+      return (fs.readFileSync(file, { encoding: 'utf-8' }).toString())
+    },
+    lastModified: () => { 
+      return fs.statSync(file).mtimeMs.toString()
+    },
+    jsonmap: () => { 
+      return readFileAsJSONMap(obj.text())
+    },
+  }
+  return obj;
 }
 let webDIR = `${baseDIR}/sub/web`;
 let nodeDIR = `${baseDIR}/sub/node`;
 let overwrittenFile = `${webDIR}/overwrriten/id-overwrite.json`;
-let overloadMap = readFileAsJSONMap(getFile(overwrittenFile)); // replace with appropriate function
-console.log(overloadMap)
-
-
-if (true) {
-  return;
-}
+let overloadMap = readFileAsJSONMap(getFile(overwrittenFile).text()); // replace with appropriate function
 
 let searchItems = [
   {
@@ -66,17 +77,84 @@ let searchItems = [
       dir: "/Users/jerrylai/Documents/PersonalProjects/denote-be/pal/work7z/src/main/java/com"
   }
 ];
-let allKEYMAP = {};
 
-searchItems.forEach(eachItem => {
-  let usedKeyMap = {};
-  let dir = getFile(eachItem.dir); // replace with appropriate function
-  let crtMap_zhCN = {};
-  let crtMap_zhHK = {};
-  let bb = getFile(`${webDIR}/overwrriten/id-overwrite.json`); // replace with appropriate function
-  let enUSOverwrite = getFile(`${webDIR}/overwrriten/enUS-overwrite.json`); // replace with appropriate function
-  let enUSMap = readFileAsJSONMap(enUSOverwrite); // replace with appropriate function
-  let allFiles = bb.lastModified() + enUSOverwrite.lastModified() + '';
-  
-  // ... rest of your code, replacing Groovy-specific functions with JavaScript equivalents ...
-});
+
+
+let scan = async () => {
+
+  let allKEYMAP = {};
+
+  for (let eachItem of searchItems) {
+    let usedKeyMap = {};
+    let dir = getFile(eachItem.dir); // replace with appropriate function
+    let crtMap_zhCN = {};
+    let crtMap_zhHK = {};
+    let idOverwriteJSONFile = getFile(`${webDIR}/overwrriten/id-overwrite.json`); // replace with appropriate function
+    let enUSOverwriteJSONFile = getFile(`${webDIR}/overwrriten/enUS-overwrite.json`); // replace with appropriate function
+    let enUSMap = readFileAsJSONMap(enUSOverwriteJSONFile.text()); // replace with appropriate function 
+    let lastModifiedForIdOverwriteJSONFile = idOverwriteJSONFile.lastModified() + enUSOverwriteJSONFile.lastModified() + '' + dir.lastModified()
+
+    // iterate all files for dir.file, recursively
+    let iterateFiles = (dir, done) => {
+      let results = [];
+      fs.readdir(dir, function (err, list) {
+        if (err) return done(err);
+        var pending = list.length;
+        if (!pending) return done(null, results);
+        list.forEach(function (file) {
+          file = path.resolve(dir, file);
+          fs.stat(file, function (err, stat) {
+            if (stat && stat.isDirectory()) {
+              iterateFiles(file, function (err, res) {
+                results = results.concat(res);
+                if (!--pending) done(null, results);
+              });
+            } else {
+              results.push(file);
+              if (!--pending) done(null, results);
+            }
+          });
+        });
+      });
+    };
+    // get all files
+    let allFiles = await new Promise((resolve, reject) => {
+      iterateFiles(dir.file, (err, results) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(results);
+        }
+      });
+    });
+    let waitTranslateObj = {}
+    // iterate all files
+    for (let eachFile of allFiles) {
+      let file = getFile(eachFile); // replace with appropriate function
+      let text = file.text();
+      let match;
+      while ((match = eachItem.pattern.exec(text))) {
+        let key = match[2];
+        let value = match[4];
+        console.log('key is '+key, ', value is ',value)
+        if (value) {
+          waitTranslateObj[key] = value;
+          // crtMap_zhCN[key] = value;
+          // crtMap_zhHK[key] = value;
+        }
+        // substring
+        text = text.substring(match.index + match[0].length);
+      }
+    }
+    
+  }
+      
+  await sleep(2000)
+
+  setTimeout(scan, 0)
+}
+
+
+setTimeout(scan, 0)
+
+
