5c8a9107052b37115a164aad5d3a43a7996099f4
LafTools Team - Ubuntu <work7z@outlook.com>
2023-12-19 01:18:34 +0800

    Add translation buttons and adjust layout in Translator component

--- sub/web/src/pages/FixedWorkBench/WB_CenterStage/nav/bottom/Translator.tsx
+++ sub/web/src/pages/FixedWorkBench/WB_CenterStage/nav/bottom/Translator.tsx
@@ -1,19 +1,35 @@
+import { Button } from "@blueprintjs/core"
 import GenCodeMirror from "../../../../../components/GenCodeMirror"
+import { Dot } from "../../../../../utils/TranslationUtils"
+import { VAL_CSS_TAB_TITLE_PANEL } from "../../../definitions/WB_Types"
 
-type SrcTarget='source'|'target'
+type SrcTarget = 'source' | 'target'
 
-let EachTranslatorBlock = (props:{type:SrcTarget})=>{
+let EachTranslatorBlock = (props: { type: SrcTarget }) => {
   return (
-    <div className="w-50p inline-block">
-      <div>{props.type}</div>
+    <div className="h-full w-50p inline-block ">
+      <div style={{
+        height: VAL_CSS_TAB_TITLE_PANEL
+      }} className=" px-2 flex flex-col justify-between align-middle">
+        <div>
+          <Button small minimal  text={props.type == 'source' ? Dot("jJuNz", "Source Language") : Dot("TwFcr", "Target Language")}></Button>
+        </div>
+      </div>
+      <div className="w-full" style={{
+          height: `calc(100% - ${VAL_CSS_TAB_TITLE_PANEL})`,
+          maxHeight: `calc(100% - ${VAL_CSS_TAB_TITLE_PANEL})`,
+          // overflow:'auto'
+      }}>
       <GenCodeMirror lineWrap={true} bigTextId="ksdk1" key={''}></GenCodeMirror>
+
+      </div>
     </div>
 
   )
 }
 
-export default  () => {
-  return <div className="flex flex-row h-full w-full p-4">
+export default () => {
+  return <div className="flex flex-row h-full w-full p-4 overflow-auto">
     <EachTranslatorBlock type='source'></EachTranslatorBlock>
     <EachTranslatorBlock type='target'></EachTranslatorBlock>
   </div>
