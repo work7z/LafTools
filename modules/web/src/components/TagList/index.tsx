1138d2cfdfb44cc8f213821cf74ec1d049956867
LafTools Team - FX <work7z@outlook.com>
2024-01-12 00:22:50 +0800

    Add new tags to TagList component

--- modules/web/src/components/TagList/index.tsx
+++ modules/web/src/components/TagList/index.tsx
@@ -1,17 +1,90 @@
 import { Tag } from "@blueprintjs/core"
 import { Dot } from "../../utils/TranslationUtils"
-
-
+import {
+    Callout,
+    PanelStack,
+    ProgressBar,
+    AnchorButton,
+    Tooltip,
+    Dialog,
+    Drawer,
+    Overlay,
+    Alert,
+    RadioGroup,
+    MenuItem,
+    Radio,
+    ButtonGroup,
+    TextArea,
+    HotkeysProvider,
+    Intent,
+    Position,
+    Toaster,
+    Checkbox,
+    NumericInput,
+    FormGroup,
+    HTMLSelect,
+    ControlGroup,
+    InputGroup,
+    Navbar,
+    NavbarHeading,
+    NonIdealState,
+    NavbarDivider,
+    NavbarGroup,
+    Alignment,
+    Classes,
+    Icon,
+    Card,
+    Elevation,
+    Button,
+    Popover,
+    Menu,
+    MenuDivider,
+} from "@blueprintjs/core";
+import AlertUtils from "../../utils/AlertUtils";
+export type TagType = {
+    id: string;
+    name: string
+    icon: string
+}
 
 // provide React components TagList based on blueprintjs framework
-let TagList = () => {
-    return <div>
-        <Tag
-            icon="tag"
-            intent="primary"
-            interactive={true}
-            children={Dot("KgTdh", "Test")}
-        ></Tag>
-    </div>
+let TagList = (props: {
+    tags: TagType[]
+    onNewTag: (name: string) => void,
+    activeTag?: string
+    setActiveTag: (tag: string) => void
+}) => {
+    return <div className="py-0 space-x-2">
+        {props.tags.map((e, i) => {
+            let isactive = e.id == props.activeTag
+            return <Tag
+                icon={e.icon as any}
+                intent={isactive ? "primary" : "none"}
+                onClick={() => {
+                    props.setActiveTag(e.id)
+                }}
+                minimal={true || !isactive}
+                interactive={true}
+                children={e.name}
+            ></Tag>
+        })}
+        <Tag minimal icon="add" onClick={() => {
+            let tid = "s89rM"
+            AlertUtils.win_prompt({
+                id: tid,
+                msg: Dot("eqkqd4W", "Please provide a name for the new tag"),
+                fn(yesOrNo: boolean, obj) {
+                    if (!obj?.iptIfHave) {
+                        AlertUtils.popError(new Error(Dot("JA_Bp", "Please provide a name for the new tag at first.")))
+                        return
+                    }
+                    if (yesOrNo) {
+                        props.onNewTag(obj?.iptIfHave)
+                        AlertUtils.deletePromptList(tid);
+                    }
+                },
+            })
+        }} interactive intent="success"></Tag>
+    </div >
 }
 export default TagList
--- modules/web/src/pages/WorkBench/FixedLayout/Main/Center/sub/center-view/notes/ShellGroupNote/index.tsx
+++ modules/web/src/pages/WorkBench/FixedLayout/Main/Center/sub/center-view/notes/ShellGroupNote/index.tsx
@@ -3,11 +3,42 @@ import gutils from "../../../../../../../../../utils/GlobalUtils";
 import { InputGroup } from "@blueprintjs/core";
 import { Dot } from "../../../../../../../../../utils/TranslationUtils";
 import GenCodeMirror from "../../../../../../../../../components/GenCodeMirror";
-import TagList from "../../../../../../../../../components/TagList";
+import TagList, { TagType } from "../../../../../../../../../components/TagList";
+import React from "react";
 
 // extension: .shg
 
 export default () => {
+    let [tags, setTags] = React.useState<TagType[]>([
+        // list of tags: SIT2, UAT2, PROD2, DB1, DB2
+        {
+            id: "sit2",
+            name: "SIT2",
+            icon: "application"
+        },
+        {
+            id: "uat2",
+            name: "UAT2",
+            icon: "application"
+        },
+        {
+            id: "prod2",
+            name: "PROD2",
+            icon: "application"
+        },
+        {
+            id: "db1",
+            name: "DB1",
+            icon: "database"
+        },
+        {
+            id: "db2",
+            name: "DB2",
+            icon: "database"
+        }
+    ])
+    let [activeTag, setActiveTag] = React.useState(tags[0].id)
+
     return <div className="w100 h100">
         <Allotment ref={e => {
             gutils.ExposureIt("allot_e", e)
@@ -19,7 +50,13 @@ export default () => {
                 <div className="p-2">
                     <div className="space-y-2">
                         <InputGroup leftIcon="search" placeholder={Dot("eqkd4W", "Filter shell commands")}></InputGroup>
-                        <TagList></TagList>
+                        <TagList onNewTag={(name: string) => {
+                            setTags([...tags, {
+                                id: name,
+                                name: name,
+                                icon: "application"
+                            }])
+                        }} activeTag={activeTag} setActiveTag={setActiveTag} tags={tags}></TagList>
                     </div>
                 </div>
             </Allotment.Pane>
