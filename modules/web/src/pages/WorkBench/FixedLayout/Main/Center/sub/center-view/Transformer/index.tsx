148a7e188f64edacf5c3a556e8c1dc976b59bd87
LafTools Team - FX <work7z@outlook.com>
2024-01-27 12:30:44 +0800

    Fix import statement in Transformer component

--- modules/web/src/pages/WorkBench/FixedLayout/Main/Center/sub/center-view/Transformer/index.tsx
+++ modules/web/src/pages/WorkBench/FixedLayout/Main/Center/sub/center-view/Transformer/index.tsx
@@ -18,7 +18,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import { Alignment, Button, ButtonProps, Navbar, Tab, Tabs, Tooltip } from "@blueprintjs/core";
+import { Alignment, Button, ButtonProps, Navbar, OL, Tab, Tabs, Tooltip } from "@blueprintjs/core";
 import GenCodeMirror from "../../../../../../../../components/GenCodeMirror";
 import {
   VAL_CSS_TAB_TITLE_PANEL,
@@ -291,6 +291,42 @@ export default (props: CommonTransformerProps) => {
     <Allotment.Pane>
       <ProcessPanel crtRuntimeStatus={crtRuntimeStatus} {...commonPassProp}></ProcessPanel>
     </Allotment.Pane>
+  if (loadError) {
+    let solution = ''
+    return <div className=" w-full h-full ">
+      <div className="w-full h-full overflow-auto  bg-blue-900 p-2">
+        <div className="bg-blue-800 w-full h-full rounded-lg p-8 shadow-lg space-y-4 text-white">
+          {/* <FaBug className="text-red-500 w-16 h-16 mb-4" /> */}
+          <h2 className="text-lg font-bold">{Dot("yt7kX2fPQ", "Caught an Unexpected Exception")}</h2>
+          <p className="text-md">{loadError}</p>
+          <hr className="border-t-2 border-gray-400 my-8" />
+          <div className="text-sm">{Dot("NvOqE3B9Vxd", "Apologize for this runtime exception, below are the possible underlying causes")}:</div>
+          <pre className="text-xs whitespace-pre-wrap break-all">
+            <OL className="list-item">
+              <li>{Dot("hAH_IjzGN", "This is an insider version, this part is still under development.")}</li>
+              <li>{Dot("hGA96yTG4", "Some features are not supported in this platform. ")}</li>
+              <li>{Dot("BCySSDEKL", "There might be a compatibility issue with your current software version.")}</li>
+              <li>{Dot("LNvUuKmHR", "The application is missing essential dependencies or libraries.")}</li>
+              <li>{Dot("_-JigU2Fg", "An unexpected input or data format has caused the exception.")}</li>
+            </OL>
+          </pre>
+          <div className="text-sm">{Dot("NvOqE3B9Vxd", "And below are the possible solutions you could refer to")}:</div>
+          <pre className="text-xs whitespace-pre-wrap break-all">
+            <OL className="list-item">
+              <li>{Dot("oFyrCkBfqy", "Raise an Issue for this error to let LafTools teams investigate it further.")}</li>
+              <li>
+                {Dot("U5Eiy7ZRZ", "Send an email to our mailbox {0} to report this error if it's related to sensitive information.", 'work7z@outlook.com')}
+              </li>
+            </OL>
+          </pre>
+
+        </div>
+      </div>
+      {/* <h2>        {Dot("RO4ZP", "An Error Occurred")}</h2><br /><div className="bp5-text-small bp5-text-secondary">
+        {loadError}</div>
+         */}
+    </div>
+  }
   return (
     <div key={sessionId} className="w-full h-full relative">
       <ControlBar
