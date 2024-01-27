94cdefe73c1b8ffa8bfb88588706aee9085efd00
LafTools Team - FX <work7z@outlook.com>
2024-01-27 12:41:04 +0800

    Refactor error handling in Transformer component

--- modules/web/src/pages/WorkBench/FixedLayout/Main/Center/sub/center-view/Transformer/index.tsx
+++ modules/web/src/pages/WorkBench/FixedLayout/Main/Center/sub/center-view/Transformer/index.tsx
@@ -293,7 +293,7 @@ export default (props: CommonTransformerProps) => {
     </Allotment.Pane>
   if (loadError) {
     let solution = ''
-    return <div className=" w-full h-full ">
+    return <div className=" w-full h-full native-style">
       <div className="w-full h-full  bg-blue-900 p-2">
         <div className="bg-blue-800 w-full h-full overflow-auto  rounded-lg p-8 shadow-lg space-y-4 text-white">
           {/* <FaBug className="text-red-500 w-16 h-16 mb-4" /> */}
@@ -302,22 +302,22 @@ export default (props: CommonTransformerProps) => {
           <hr className="border-t-2 border-gray-400 my-8" />
           <div className="text-sm">{Dot("NvOqE3B9Vxd", "Apologize for this runtime exception, below are the possible underlying causes")}:</div>
           <pre className="text-xs whitespace-pre-wrap break-all">
-            <OL className="list-item">
+            <ul className="list-item ">
               <li>{Dot("hAH_IjzGN", "This is an insider version, this part is still under development.")}</li>
               <li>{Dot("hGA96yTG4", "Some features are not supported in this platform. ")}</li>
               <li>{Dot("BCySSDEKL", "There might be a compatibility issue with your current software version.")}</li>
               <li>{Dot("LNvUuKmHR", "The application is missing essential dependencies or libraries.")}</li>
               <li>{Dot("_-JigU2Fg", "An unexpected input or data format has caused the exception.")}</li>
-            </OL>
+            </ul>
           </pre>
           <div className="text-sm">{Dot("NvOqE3B9Vxd", "And below are the possible solutions you could refer to")}:</div>
           <pre className="text-xs whitespace-pre-wrap break-all">
-            <OL className="list-item">
+            <ul className="list-item">
               <li>{Dot("j5ttfMZZE", "Raise an Issue for this error to let LafTools team investigate it further.")}</li>
               <li>
                 {Dot("U5Eiy7ZRZ", "Send an email to LafTools team({0}) to report this error if it's related to sensitive information.", 'work7z@outlook.com')}
               </li>
-            </OL>
+            </ul>
           </pre>
 
         </div>
