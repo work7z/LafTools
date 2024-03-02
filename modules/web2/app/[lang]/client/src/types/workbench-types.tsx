710cbf5cf57b07e2306d7945c1ddae616ae0e78f
LafTools Team - FX <work7z@outlook.com>
2024-03-02 15:54:11 +0800

    Fix comment formatting in workbench-types.tsx

--- modules/web2/app/[lang]/client/src/types/workbench-types.tsx
+++ modules/web2/app/[lang]/client/src/types/workbench-types.tsx
@@ -5,7 +5,7 @@
 // Description:
 // Copyright (C) 2023 - Present, https://codegen.cc
 //
-// This program is free software: you can redistribute it and/or modify
+// This     program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as
 // published by the Free Software Foundation, either version 3 of the
 // License, or (at your option) any later version.
