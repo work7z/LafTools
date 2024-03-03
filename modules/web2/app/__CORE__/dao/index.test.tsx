997704a4ddbdfefe73dfe1bdad784b39c7e530fc
LafTools Team - FX <work7z@outlook.com>
2024-03-03 10:32:57 +0800

    Refactor table creation and maintenance tests

--- modules/web2/app/__CORE__/dao/index.test.tsx
+++ modules/web2/app/__CORE__/dao/index.test.tsx
@@ -23,6 +23,7 @@ import fn from './index'
 import { expect, test } from 'vitest'
 import { render, screen } from '@testing-library/react'
 import { DataTypes, Model } from 'sequelize'
+import model from './model';
 
 class TestUser extends Model {
     declare id: number;
@@ -43,54 +44,54 @@ test('Test Conn', async () => {
     let sequelize = daoRef.db
 })
 
-test('Test Create and Mantain Table', async () => {
+test('Test Create and Mantain Table2', async () => {
     let daoRef = await fn()
+    await model(daoRef)
+    // let sequelize = daoRef.db
+    // // sequelize.sync({ alter: true, })
 
-    let sequelize = daoRef.db
-    sequelize.sync({alter:true,})
+    // TestUser.init({
+    //     id: {
+    //         type: DataTypes.INTEGER,
+    //         autoIncrement: true,
+    //         primaryKey: true
+    //     }
+    // }, { sequelize })
 
-    TestUser.init({
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        }
-    }, { sequelize })
-
-    const user = new TestUser({ id: 1 });
-    user.id; // 1
+    // const user = new TestUser({ id: 1 });
+    // user.id; // 1
 })
 
 // more cases for sequlize
 test('Test Create and Mantain Table', async () => {
     let daoRef = await fn()
 
-    let sequelize = daoRef.db
-    sequelize.sync({alter:true})
+    // let sequelize = daoRef.db
+    // sequelize.sync({ alter: true })
 
-    TestUser.init({
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        }
-    }, { sequelize })
+    // TestUser.init({
+    //     id: {
+    //         type: DataTypes.INTEGER,
+    //         autoIncrement: true,
+    //         primaryKey: true
+    //     }
+    // }, { sequelize })
 
 
-    let usersall = await TestUser.findAll();
-    usersall.forEach(x=>{
-        // x.destroy()
-    })
+    // let usersall = await TestUser.findAll();
+    // usersall.forEach(x => {
+    //     // x.destroy()
+    // })
 
-    const user = new TestUser({ id: 1 });
-    user.id; // 1
-    user.save()
+    // const user = new TestUser({ id: 1 });
+    // user.id; // 1
+    // user.save()
 
-    let users = await TestUser.findAll();
-    expect(users.length).toBe(1)
+    // let users = await TestUser.findAll();
+    // expect(users.length).toBe(1)
 
-    let user1 = users[0]
-    expect(user1.id).toBe(1)
+    // let user1 = users[0]
+    // expect(user1.id).toBe(1)
 
 
     // let user2 = new TestUser()
@@ -110,6 +111,5 @@ test('Test Create and Mantain Table', async () => {
     // expect(users.length).toBe(1)
 
     // user1.destroy()
-
 })
 
