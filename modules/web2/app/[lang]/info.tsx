349c24648131c3bab4b29972bab5258bbb5528af
LafTools Team - FX <work7z@outlook.com>
2024-03-06 22:22:30 +0800

    Update version and timestamp in info.tsx and remove unused dependencies in package.json

--- modules/web2/app/[lang]/info.tsx
+++ modules/web2/app/[lang]/info.tsx
@@ -4,6 +4,6 @@ import { AppInfoClz } from "./types"
 export default {
   "version": "v2.0.6",
   "releaseDate": "2024-03-06",
-  "timestamp": "1709734347"
+  "timestamp": "1709734891"
 } satisfies AppInfoClz
 
