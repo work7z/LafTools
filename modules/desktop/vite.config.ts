b3e2c590b2cede2ec453926ee3ba6d556aa3f0aa
LafTools Team - FX <work7z@outlook.com>
2024-03-07 22:15:22 +0800

    Remove unused icons and files, update dependencies

--- modules/desktop/vite.config.ts
+++ modules/desktop/vite.config.ts
@@ -2,20 +2,30 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig(async () => ({
+export default defineConfig({
   plugins: [react()],
-
-  // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
-  //
-  // 1. prevent vite from obscuring rust errors
+  // prevent vite from obscuring rust errors
   clearScreen: false,
-  // 2. tauri expects a fixed port, fail if that port is not available
+  // Tauri expects a fixed port, fail if that port is not available
   server: {
-    port: 1420,
     strictPort: true,
-    watch: {
-      // 3. tell vite to ignore watching `src-tauri`
-      ignored: ["**/src-tauri/**"],
-    },
   },
-}));
+  // to access the Tauri environment variables set by the CLI with information about the current target
+  envPrefix: [
+    "VITE_",
+    "TAURI_PLATFORM",
+    "TAURI_ARCH",
+    "TAURI_FAMILY",
+    "TAURI_PLATFORM_VERSION",
+    "TAURI_PLATFORM_TYPE",
+    "TAURI_DEBUG",
+  ],
+  build: {
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target: process.env.TAURI_PLATFORM == "windows" ? "chrome105" : "safari13",
+    // don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? "esbuild" : false,
+    // produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
+});
