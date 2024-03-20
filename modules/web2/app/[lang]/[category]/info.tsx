7b5825aa84866d5133f7a12c088c4ccccfc29f62
LafTools Team - FX <work7z@outlook.com>
2024-03-20 20:46:22 +0800

    Update app version and release date

--- modules/web2/app/[lang]/[category]/info.tsx
+++ modules/web2/app/[lang]/[category]/info.tsx
@@ -1,29 +1,10 @@
-// LafTools - The Leading All-In-One ToolBox for Programmers.
-// 
-// Date: Wed, 6 Mar 2024
-// Author:   
-// Description: 
-// Copyright (C) 2024 - Present, https://laftools.dev and https://codegen.cc
-//
-// This program is free software: you can redistribute it and/or modify
-// it under the terms of the GNU Affero General Public License as
-// published by the Free Software Foundation, either version 3 of the
-// License, or (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU Affero General Public License for more details.
-//
-// You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 
 import { AppInfoClz } from "@/app/__CORE__/meta/ctypes"
 
-export default {
-    "version": "v2.1.26",
-    "releaseDate": "2024-03-07",
-    "timestamp": "1709819731"
-} satisfies AppInfoClz
 
+    export default {
+    "version": "v2.1.0",
+    "releaseDate": "2024-03-20",
+    "timestamp": "1710938772"
+    } satisfies AppInfoClz
+    
