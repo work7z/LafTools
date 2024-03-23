ae77fa83e759aed5c47de26ba34ddd7208e062d5
LafTools Team - FX <work7z@outlook.com>
2024-03-23 12:29:19 +0800

    Refactor file export function in FileExportUtils.tsx

--- modules/web2/app/[lang]/client/src/utils/FileExportUtils.tsx
+++ modules/web2/app/[lang]/client/src/utils/FileExportUtils.tsx
@@ -5,22 +5,17 @@ export async function js_export_trigger({ saveValue, filename, }) {
         const blob = new Blob([data], {
             type: "application/octet-stream",
         });
-        // 创建新的URL并指向File对象或者Blob对象的地址
         const blobURL = window.URL.createObjectURL(blob);
-        // 创建a标签，用于跳转至下载链接
         const tempLink = document.createElement("a");
         tempLink.style.display = "none";
         tempLink.href = blobURL;
         tempLink.setAttribute("download", decodeURI(filename));
-        // 兼容：某些浏览器不支持HTML5的download属性
         if (typeof tempLink.download === "undefined") {
             tempLink.setAttribute("target", "_blank");
         }
-        // 挂载a标签
         document.body.appendChild(tempLink);
         tempLink.click();
         document.body.removeChild(tempLink);
-        // 释放blob URL地址
         window.URL.revokeObjectURL(blobURL);
     }
     let fileName = window.prompt(Dot("iVfYXX1C5", "Enter file name"), filename || `result-${Date.now()}.txt`);
