cd579427fc87d21584955c31966f29e8f9420295
LafTools Team - FX <work7z@outlook.com>
2024-03-28 21:29:17 +0800

    Provide server module

---
+++ modules/server/src/models/users.model.ts
@@ -0,0 +1,39 @@
+import { Sequelize, DataTypes, Model, Optional } from 'sequelize';
+import { User } from '@interfaces/users.interface';
+
+export type UserCreationAttributes = Optional<User, 'id' | 'email' | 'password'>;
+
+export class UserModel extends Model<User, UserCreationAttributes> implements User {
+  public id: number;
+  public email: string;
+  public password: string;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+export default function (sequelize: Sequelize): typeof UserModel {
+  UserModel.init(
+    {
+      id: {
+        autoIncrement: true,
+        primaryKey: true,
+        type: DataTypes.INTEGER,
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.STRING(45),
+      },
+      password: {
+        allowNull: false,
+        type: DataTypes.STRING(255),
+      },
+    },
+    {
+      tableName: 'users',
+      sequelize,
+    },
+  );
+
+  return UserModel;
+}
---
+++ modules/server/src/services/users.service.ts
@@ -0,0 +1,50 @@
+import { hash } from 'bcrypt';
+import { Service } from 'typedi';
+import { DB } from '@database';
+import { CreateUserDto } from '@dtos/users.dto';
+import { HttpException } from '@/exceptions/httpException';
+import { User } from '@interfaces/users.interface';
+
+@Service()
+export class UserService {
+  public async findAllUser(): Promise<User[]> {
+    const allUser: User[] = await DB.Users.findAll();
+    return allUser;
+  }
+
+  public async findUserById(userId: number): Promise<User> {
+    const findUser: User = await DB.Users.findByPk(userId);
+    if (!findUser) throw new HttpException(409, "User doesn't exist");
+
+    return findUser;
+  }
+
+  public async createUser(userData: CreateUserDto): Promise<User> {
+    const findUser: User = await DB.Users.findOne({ where: { email: userData.email } });
+    if (findUser) throw new HttpException(409, `This email ${userData.email} already exists`);
+
+    const hashedPassword = await hash(userData.password, 10);
+    const createUserData: User = await DB.Users.create({ ...userData, password: hashedPassword });
+    return createUserData;
+  }
+
+  public async updateUser(userId: number, userData: CreateUserDto): Promise<User> {
+    const findUser: User = await DB.Users.findByPk(userId);
+    if (!findUser) throw new HttpException(409, "User doesn't exist");
+
+    const hashedPassword = await hash(userData.password, 10);
+    await DB.Users.update({ ...userData, password: hashedPassword }, { where: { id: userId } });
+
+    const updateUser: User = await DB.Users.findByPk(userId);
+    return updateUser;
+  }
+
+  public async deleteUser(userId: number): Promise<User> {
+    const findUser: User = await DB.Users.findByPk(userId);
+    if (!findUser) throw new HttpException(409, "User doesn't exist");
+
+    await DB.Users.destroy({ where: { id: userId } });
+
+    return findUser;
+  }
+}
